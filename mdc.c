db1e42ef3ad581c91bad147dbb07c3ffa5430355
adna olívia <88201392+adnaolivia@users.noreply.github.com>
2023-04-14 10:21:16 -0300

    accepted

--- mdc.c
+++ mdc.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-int mdc(int a, int b){
-  int x;
-
-  while (b != 0)
-  {
-    x = a % b;
-    a = b;
-    b = x;
-  }
+int mdc(int a, int b) {
+    int resto;
+    while (b != 0)
+        resto = a % b;
+        a = b;
+        b = resto;
     return a;
 }
-
-int main(){
-  int n1, n2, op;
-  scanf("%d %d", &n1, &n2);
-  op = mdc(n1, n2);
-
-  printf("%d\n", op);
-  return 0;
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", mdc(a, b));
+    return 0;
 }
